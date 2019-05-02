fe3f381176d56f5d73dd9fd14d19f6b494b0cbd7
M. Ridwan Zalbina <[zalbinaridwan@gmail.com]>
2019-05-03 05:25:17 +0700

    all perubahan

---
+++ application/logs/log-2019-05-02.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-05-02 16:53:04 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) /opt/lampp/htdocs/pubmsumsel/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-05-02 16:53:04 --> Unable to connect to the database
+ERROR - 2019-05-02 16:53:25 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) /opt/lampp/htdocs/pubmsumsel/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-05-02 16:53:25 --> Unable to connect to the database
