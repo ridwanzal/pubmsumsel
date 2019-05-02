70c1c1c3000078dbded05bc35374a2cb310655e0
M. Ridwan Zalbina <[zalbinaridwan@gmail.com]>
2019-05-02 10:59:25 +0700

    first commit

---
+++ application/logs/log-2019-04-29.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-04-29 17:19:28 --> Severity: Notice --> unserialize(): Error at offset 186 of 1352 bytes /opt/lampp/htdocs/dpusumsel/system/libraries/Cache/drivers/Cache_file.php 275
+ERROR - 2019-04-29 17:19:28 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/dpusumsel/system/libraries/Cache/drivers/Cache_file.php 106
---
+++ application/logs/log-2019-04-30.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-04-30 05:07:02 --> 404 Page Not Found: Uploads/bidang
