bb80f9f575c2853de8bd122b57685c41e18fda1a
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-08-01 02:27:26 +0700

    add zip download file

--- application/controllers/AN_admin.php
+++ application/controllers/AN_admin.php
@@ -1,6 +1,8 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+require_once APPPATH.'third_party/fpdf/fpdf.php';
+
 class AN_admin extends AN_Apricot {
 	protected $login=false;
 	//user data
@@ -16,11 +18,9 @@ class AN_admin extends AN_Apricot {
 
 	function __construct(){
 		parent::__construct();
-
-
-	//session_start();
-		//$_SESSION["test"]="nando";
-
+		$pdf = new FPDF();
+		$pdf->AddPage();
+    $this->load->model('admin/pengujian_model', 'pengujian');
 
 		//Panggil database
 		$this->load->database();
@@ -1385,23 +1385,50 @@ class AN_admin extends AN_Apricot {
 
 	public function download_file($id)
 	{
+		// pdf
+		$data = $this->db->get_where('pengujian', ['id'  => $id])->result_array();
+
+    $pdf = new FPDF('L','mm','A4'); //L = lanscape P= potrait
+    // membuat halaman baru
+    $pdf->AddPage();
+    // setting jenis font yang akan digunakan
+    $pdf->SetFont('Arial','B',16);
+    $ya = 44;
+    // mencetak string
+    $pdf->Cell(280,7,'Data Pengujian | Dinas PUBMTR Sumatera Selatan',0,1,'C');
+    $pdf->SetFont('Arial','B',12);
+    // Memberikan space kebawah agar tidak terlalu rapat
+    $pdf->Cell(10,7,'',0,1);
+    $pdf->SetFont('Arial','B',10);
+    $pdf->Cell(40,6,'NO KTP',1,0);
+    $pdf->Cell(40,6,'NAMA',1,0);
+    $pdf->Cell(40,6,'EMAIL',1,0);
+    $pdf->Cell(30,6,'NO HP',1,0);
+    $pdf->Cell(80,6,'JENIS PENGUJIAN',1,0);
+    $pdf->Cell(50,6,'TANGGAL DIAJUKAN',1,1);
+    $pdf->SetFont('Arial','',10);
+    foreach ($data as $row){
+        $pdf->Cell(40,6,$row['no_ktp'],1,0);
+        $pdf->Cell(40,6,$row['nama'],1,0);
+        $pdf->Cell(40,6,$row['email'],1,0);
+        $pdf->Cell(30,6,$row['no_hp'],1,0);
+        $pdf->Cell(80,6,$row['jenis_pengujian'],1,0);
+        $pdf->Cell(50,6,$row['created_at'],1,1);
+    }
+
+
+		// pdf
+		$this->load->library('zip');
 		$this->load->model('admin/pengujian_model', 'pengujian');
 		$user = $this->db->get_where('pengujian', ['id' => $id])->result();
 
 		$zip = new ZipArchive();
-		$file = base_url('/assets/uploads/surat/' . $user[0]->surat);
+		$pdf->Output(FCPATH . '/assets/uploads/pengujian/data_' . $user[0]->nama . '_' . $id . '.pdf', 'F');
 
-		$tmp_file = tempnam('.','');
-		$zip->open($tmp_file, ZipArchive::CREATE);
+		$this->zip->read_file(FCPATH . '/assets/uploads/surat/' . $user[0]->surat);
+		$this->zip->read_file(FCPATH . '/assets/uploads/pengujian/data_' . $user[0]->nama . '_' . $id . '.pdf');
 
-		$download_file = file_get_contents($file);
-		$zip->addFromString(basename($file),$download_file);
-
-		$zip->close();
-
-		header('Content-disposition: attachment; filename=Dokumen_pengujian_' . $user[0]->id . '_' . $user[0]->nama . '.zip');
-		header('Content-type: application/zip');
-		readfile($tmp_file);
+		$this->zip->download('Data pengujian ' . $user[0]->nama . '.zip');
 	}
 
 	public function pengujian()
--- application/logs/log-2020-07-31.php
+++ application/logs/log-2020-07-31.php
@@ -36,3 +36,83 @@ ERROR - 2020-07-31 20:16:12 --> 404 Page Not Found: An-theme/admin
 ERROR - 2020-07-31 20:18:06 --> 404 Page Not Found: An-theme/admin
 ERROR - 2020-07-31 20:18:06 --> 404 Page Not Found: An-theme/admin
 ERROR - 2020-07-31 20:18:07 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:20:19 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:20:19 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:20:20 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:37:05 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:37:06 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:37:06 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:37:07 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:37:08 --> Severity: error --> Exception: Class 'FPDF' not found C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1392
+ERROR - 2020-07-31 20:37:32 --> Severity: error --> Exception: Class 'FPDF' not found C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 19
+ERROR - 2020-07-31 20:43:22 --> Severity: error --> Exception: FPDF error: Incorrect output destination: http://localhost/pubmsumsel/assets/uploads/pengujian/2 C:\xampp\htdocs\pubmsumsel\application\third_party\fpdf\fpdf.php 271
+ERROR - 2020-07-31 20:43:54 --> Severity: error --> Exception: FPDF error: Incorrect output destination: http://localhost/pubmsumsel/assets/uploads/pengujian/2 C:\xampp\htdocs\pubmsumsel\application\third_party\fpdf\fpdf.php 271
+ERROR - 2020-07-31 20:44:10 --> Severity: error --> Exception: FPDF error: Incorrect output destination: http://localhost/pubmsumsel/assets/uploads/pengujian/2 C:\xampp\htdocs\pubmsumsel\application\third_party\fpdf\fpdf.php 271
+ERROR - 2020-07-31 20:45:12 --> Severity: Warning --> file_put_contents(http://localhost/pubmsumsel/assets/uploads/pengujian/2): failed to open stream: HTTP wrapper does not support writeable connections C:\xampp\htdocs\pubmsumsel\application\third_party\fpdf\fpdf.php 1021
+ERROR - 2020-07-31 20:45:12 --> Severity: error --> Exception: FPDF error: Unable to create output file: http://localhost/pubmsumsel/assets/uploads/pengujian/2 C:\xampp\htdocs\pubmsumsel\application\third_party\fpdf\fpdf.php 271
+ERROR - 2020-07-31 20:45:57 --> Severity: error --> Exception: syntax error, unexpected ',' C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1427
+ERROR - 2020-07-31 20:46:12 --> Severity: error --> Exception: syntax error, unexpected ',' C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1427
+ERROR - 2020-07-31 20:46:34 --> Severity: Warning --> file_put_contents(/assets/uploads/pengujian/2): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\third_party\fpdf\fpdf.php 1021
+ERROR - 2020-07-31 20:46:34 --> Severity: error --> Exception: FPDF error: Unable to create output file: /assets/uploads/pengujian/2 C:\xampp\htdocs\pubmsumsel\application\third_party\fpdf\fpdf.php 271
+ERROR - 2020-07-31 20:47:36 --> Severity: Warning --> file_put_contents(../assets/uploads/pengujian/2): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\third_party\fpdf\fpdf.php 1021
+ERROR - 2020-07-31 20:47:36 --> Severity: error --> Exception: FPDF error: Unable to create output file: ../assets/uploads/pengujian/2 C:\xampp\htdocs\pubmsumsel\application\third_party\fpdf\fpdf.php 271
+ERROR - 2020-07-31 20:54:38 --> Severity: Notice --> Undefined variable: files C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1439
+ERROR - 2020-07-31 20:54:38 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1439
+ERROR - 2020-07-31 20:54:38 --> Severity: Warning --> filesize(): stat failed for 2_Sultan Muhammad.zip C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1449
+ERROR - 2020-07-31 20:54:38 --> Severity: Warning --> readfile(2_Sultan Muhammad.zip): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 20:54:56 --> Severity: Notice --> Undefined variable: files C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1439
+ERROR - 2020-07-31 20:54:56 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1439
+ERROR - 2020-07-31 20:54:56 --> Severity: Warning --> filesize(): stat failed for 2_Sultan Muhammad.zip C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1449
+ERROR - 2020-07-31 20:54:56 --> Severity: Warning --> readfile(2_Sultan Muhammad.zip): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 20:55:00 --> Severity: Notice --> Undefined variable: files C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1439
+ERROR - 2020-07-31 20:55:00 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1439
+ERROR - 2020-07-31 20:55:00 --> Severity: Warning --> filesize(): stat failed for 2_Sultan Muhammad.zip C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1449
+ERROR - 2020-07-31 20:55:00 --> Severity: Warning --> readfile(2_Sultan Muhammad.zip): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 20:56:28 --> 404 Page Not Found: Assets/uploads
+ERROR - 2020-07-31 20:56:28 --> Severity: Warning --> file_get_contents(http://localhost/pubmsumsel/assets/uploads/pengujain/2.pdf): failed to open stream: HTTP request failed! HTTP/1.0 404 Not Found
+ C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1440
+ERROR - 2020-07-31 20:56:28 --> Severity: Warning --> filesize(): stat failed for 2_Sultan Muhammad.zip C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1449
+ERROR - 2020-07-31 20:56:28 --> Severity: Warning --> readfile(2_Sultan Muhammad.zip): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 20:57:05 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:57:05 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:57:05 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:57:06 --> Severity: Warning --> filesize(): stat failed for 2_Sultan Muhammad.zip C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1449
+ERROR - 2020-07-31 20:57:06 --> Severity: Warning --> readfile(2_Sultan Muhammad.zip): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 20:57:49 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:57:49 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:57:50 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:57:51 --> Severity: Warning --> filesize(): stat failed for 2_Sultan Muhammad C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1449
+ERROR - 2020-07-31 20:57:51 --> Severity: Warning --> readfile(2_Sultan Muhammad): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 20:58:33 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:58:34 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:58:34 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:58:35 --> Severity: Warning --> filesize(): stat failed for Dokumen_pengujian_2_Sultan Muhammad C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1449
+ERROR - 2020-07-31 20:58:35 --> Severity: Warning --> readfile(Dokumen_pengujian_2_Sultan Muhammad): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 20:59:31 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:59:32 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:59:32 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:59:32 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:59:42 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:59:42 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 20:59:44 --> Severity: Warning --> filesize(): stat failed for Dokumen_pengujian_2_Sultan Muhammad.zip C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1449
+ERROR - 2020-07-31 20:59:44 --> Severity: Warning --> readfile(Dokumen_pengujian_2_Sultan Muhammad.zip): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 21:01:35 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:01:36 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:01:36 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:01:37 --> Severity: Warning --> readfile(Dokumen_pengujian_2_Sultan Muhammad.zip): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 21:06:45 --> Severity: Warning --> filesize(): stat failed for C:\xampp\htdocs\pubmsumsel\assets/uploads/zip/Dokumen_pengujian_2_Sultan Muhammad.zip C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1449
+ERROR - 2020-07-31 21:06:46 --> Severity: Warning --> readfile(C:\xampp\htdocs\pubmsumsel\assets/uploads/zip/Dokumen_pengujian_2_Sultan Muhammad.zip): failed to open stream: No such file or directory C:\xampp\htdocs\pubmsumsel\application\controllers\AN_admin.php 1450
+ERROR - 2020-07-31 21:15:26 --> Severity: Warning --> file_exists() expects parameter 1 to be a valid path, array given C:\xampp\htdocs\pubmsumsel\system\libraries\Zip.php 316
+ERROR - 2020-07-31 21:23:31 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:23:56 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:23:56 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:23:57 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:24:33 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:24:33 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:24:34 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:26:06 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:26:06 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:26:07 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:27:09 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:27:09 --> 404 Page Not Found: An-theme/admin
+ERROR - 2020-07-31 21:27:09 --> 404 Page Not Found: An-theme/admin
